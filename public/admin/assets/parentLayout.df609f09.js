f5e8dd94c45a391ed62ed5d38e0e241791b5e0e4
AOAOSTAR <86001674+aoaostar@users.noreply.github.com>
2022-09-11 20:49:46 +0800

    style: 💄 update ota view

--- public/admin/assets/parentLayout.df609f09.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,a0 as r,e as t,x as n}from"./index.5d02c90a.js";const a={};function c(_,s){const e=r("router-view");return t(),n(e)}var f=o(a,[["render",c]]);export{f as default};
---
+++ public/admin/assets/parentLayout.fdb459fd.js
@@ -0,0 +1,1 @@
+import{_ as o,a0 as r,e as t,x as n}from"./index.7a33fce5.js";const a={};function c(_,s){const e=r("router-view");return t(),n(e)}var f=o(a,[["render",c]]);export{f as default};
